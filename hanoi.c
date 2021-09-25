7279904a8ded16686a3a4d8ce7bfa56b841e9ece
hutnikus <hutnand@gmail.com>
2021-09-25 20:55:06 +0200

    prelozenie textu

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("move stone 1 from %d to %d\n", a, b);
+    printf("posun krment 1 z %d do %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("posun kamen %d z %d do %d\n", n, a, b);
     hanoi(n, 6 - a - b, b);
   }
 }
